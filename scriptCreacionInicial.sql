3b4672d01aef4ca244f2cfdaa6829794f636d631
Dazztian <Dazztian@users.noreply.github.com>
2019-05-24 10:25:42 -0300

    Add files via upload

---
+++ scriptCreacionInicial.sql
@@ -0,0 +1,144 @@
+USE [GD1C2019]
+GO
+
+SET QUOTED_IDENTIFIER OFF
+SET ANSI_NULLS ON 
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'VAMONIUEL')
+BEGIN
+EXEC ('CREATE SCHEMA [VAMONIUEL] AUTHORIZATION [gdCrucero2019]')
+END
+
+
+CREATE TABLE VAMONIUEL.[Rol](
+	[ID] [int] NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Nombre] [nvarchar](50) NOT NULL UNIQUE,
+	[Habilitado] [bit] default 1
+);	
+
+
+CREATE TABLE VAMONIUEL.[Funcion](
+	[ID] [int] NOT NULL PRIMARY KEY,
+	[nombre] [nvarchar](50) NOT NULL,
+);
+
+CREATE TABLE VAMONIUEL.[Rol_X_Funcion](
+	[ID_Rol] [int],
+	[ID_Funcion] [int],
+	PRIMARY KEY(ID_ROL, ID_Funcion),
+	CONSTRAINT FK_Rol FOREIGN KEY (ID_Rol) REFERENCES VAMONIUEL.[Rol](ID),
+	CONSTRAINT FK_Funcion FOREIGN KEY (ID_Funcion) REFERENCES VAMONIUEL.[Funcion](ID)
+);
+
+CREATE TABLE VAMONIUEL.[Usuario](
+	[ID] [int] NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Usuario] [nvarchar](50) NOT NULL UNIQUE,
+	[Contrasenia] [binary](32) NOT NULL,
+	[cant_accesos_fallidos] int default 0,
+	[habilitado] [bit] default 1,
+	[contrasena_autogenerada] [bit] default 0,
+);
+
+
+CREATE TABLE [VAMONIUEL].[CLIENTE]	
+(
+	[CLI_NOMBRE] [nvarchar](255) NULL,
+	[CLI_APELLIDO] [nvarchar](255) NULL,
+	[CLI_DNI] [decimal](18, 0) NULL,
+	[CLI_DIRECCION] [nvarchar](255) NULL,
+	[CLI_TELEFONO] [int] NULL,
+	[CLI_MAIL] [nvarchar](255) NULL,
+	[CLI_FECHA_NAC] [datetime2](3) NULL	
+;
+
+CREATE TABLE [VAMONIUEL].[PASAJE]
+(
+	[PASAJE_CODIGO] [decimal](18, 0) NULL,
+	[PASAJE_PRECIO] [decimal](18, 2) NULL,
+	[PASAJE_FECHA_COMPRA] [datetime2](3) NULL,
+	[FECHA_SALIDA] [datetime2](3) NULL,
+	[FECHA_LLEGADA] [datetime2](3) NULL,
+	[FECHA_LLEGADA_ESTIMADA] [datetime2](3) NULL,
+	ID_Cliente int not null,
+	CONSTRAINT FK_Pasaje_Cliente FOREIGN KEY (ID_Cliente) REFERENCES VAMONIUEL.[Cliente](ID)			
+);
+
+CREATE TABLE [VAMONIUEL].[VIAJE]
+(
+	ID int not null,
+	Origen [nvarchar](255) not null,
+	Destino [nvarchar](255) not null,
+	FechaInicio [datetime2](3) not null,
+	FechaFin [datetime2](3) not null,
+	ID_Pasaje int not null,
+	ID_Crucero	 int not null,
+	CONSTRAINT FK_Viaje_Pasaje FOREIGN KEY (ID_Pasaje) REFERENCES VAMONIUEL.[Pasaje](ID),			
+	CONSTRAINT FK_Viaje_Crucero FOREIGN KEY (ID_Crucero) REFERENCES VAMONIUEL.[Crucero](ID)			
+);
+
+CREATE TABLE [VAMONIUEL].[RECORRIDO]
+(
+	[RECORRIDO_CODIGO] [decimal](18, 0) NULL,
+	[RECORRIDO_PRECIO_BASE] [decimal](18, 2) NULL,
+	[PUERTO_DESDE] [nvarchar](255) NULL,
+	[PUERTO_HASTA] [nvarchar](255) NULL,
+	ID_Viaje int not null,
+	CONSTRAINT FK_Recorrido_Viaje FOREIGN KEY (ID_Viaje) REFERENCES VAMONIUEL.[Viaje](ID)		
+);
+
+CREATE TABLE [VAMONIUEL].[ViajeXRecorrido]
+(
+	ID INT NOT NULL,
+	ID_Viaje int not null,
+	ID_Recorrido int not null,
+	int orden not null,
+	CONSTRAINT FK_ViajeXRecorrido_Viaje FOREIGN KEY (ID_Viaje) REFERENCES VAMONIUEL.[Viaje](ID),			
+	CONSTRAINT FK_ViajeXRecorrido_Recorrido FOREIGN KEY (ID_Recorrido) REFERENCES VAMONIUEL.[RECORRIDO](ID)			
+);
+
+
+CREATE TABLE [VAMONIUEL].[Puerto]
+(	
+	ID int not null,
+	[Nombre] [nvarchar](255) NULL,
+	[Habilitado] bit not null
+);
+
+CREATE TABLE [VAMONIUEL].[RecorridoXViaje]
+(
+	ID INT NOT NULL,
+	ID_Recorrido int not null,
+	ID_Puerto int not null,
+	CONSTRAINT FK_RecorridoXViaje_Recorrido FOREIGN KEY (ID_Recorrido) REFERENCES VAMONIUEL.[Recorrid](ID),			
+	CONSTRAINT FK_RecorridoXViaje_PuertoFOREIGN KEY (ID_Puerto) REFERENCES VAMONIUEL.[Puerto](ID)	
+);
+
+
+CREATE TABLE [VAMONIUEL].[CABINA]
+(
+	[CABINA_NRO] [decimal](18, 0) NULL,
+	[CABINA_PISO] [decimal](18, 0) NULL,
+	[CABINA_TIPO] [nvarchar](255) NULL,
+	[CABINA_TIPO_PORC_RECARGO] [decimal](18, 2) NULL,	
+	ocupada bit not null,
+	ID_Crucero int not null,
+	CONSTRAINT FK_Cabina_Crucero FOREIGN KEY (ID_Crucero) REFERENCES VAMONIUEL.[Crucero](ID)		
+);
+
+CREATE TABLE [VAMONIUEL].[CRUCERO]
+(	
+	ID int not null,
+	[CRU_FABRICANTE] [nvarchar](255) NULL,
+	[CRUCERO_MODELO] [nvarchar](50) NULL,
+	[CRUCERO_IDENTIFICADOR] [nvarchar](50) NULL	
+);
+
+CREATE TABLE [VAMONIUEL].[RESERVA]
+(
+	[RESERVA_CODIGO] [decimal](18, 0) NULL,
+	[RESERVA_FECHA] [datetime2](3) NULL,
+	ID_Pasaje int not null,
+	CONSTRAINT FK_Reserva_Pasaje FOREIGN KEY (ID_Pasaje) REFERENCES VAMONIUEL.[Pasaje](ID)	
+);
+
+	
