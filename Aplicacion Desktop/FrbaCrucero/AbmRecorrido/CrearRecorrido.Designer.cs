58fb358da2c82b2d739b62eafaaef9dcd57292a7
fedeDosSantos <fededs20@gmail.com>
2019-06-04 16:09:01 -0300

    agregar tramos

--- Aplicacion Desktop/FrbaCrucero/AbmRecorrido/CrearRecorrido.Designer.cs
+++ Aplicacion Desktop/FrbaCrucero/AbmRecorrido/CrearRecorrido.Designer.cs
@@ -30,14 +30,22 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxOrigen = new System.Windows.Forms.ComboBox();
+            this.comboBoxDestino = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnAgregarTramo = new System.Windows.Forms.Button();
+            this.btnCrearRecorrido = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dataGridViewTramos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTramos)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(62, 27);
+            this.label1.Location = new System.Drawing.Point(66, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
@@ -46,39 +54,109 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 55);
+            this.label2.Location = new System.Drawing.Point(66, 99);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Destino";
             // 
-            // comboBox1
+            // comboBoxOrigen
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(152, 24);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.comboBoxOrigen.FormattingEnabled = true;
+            this.comboBoxOrigen.Location = new System.Drawing.Point(156, 68);
+            this.comboBoxOrigen.Name = "comboBoxOrigen";
+            this.comboBoxOrigen.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxOrigen.TabIndex = 2;
             // 
-            // comboBox2
+            // comboBoxDestino
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(152, 52);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 3;
+            this.comboBoxDestino.FormattingEnabled = true;
+            this.comboBoxDestino.Location = new System.Drawing.Point(156, 96);
+            this.comboBoxDestino.Name = "comboBoxDestino";
+            this.comboBoxDestino.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDestino.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Seleccione los tramos del recorrido:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Precio (pesos)";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(156, 127);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(121, 20);
+            this.txtPrecio.TabIndex = 6;
+            // 
+            // btnAgregarTramo
+            // 
+            this.btnAgregarTramo.Location = new System.Drawing.Point(196, 166);
+            this.btnAgregarTramo.Name = "btnAgregarTramo";
+            this.btnAgregarTramo.Size = new System.Drawing.Size(107, 23);
+            this.btnAgregarTramo.TabIndex = 7;
+            this.btnAgregarTramo.Text = "Agregar tramo";
+            this.btnAgregarTramo.UseVisualStyleBackColor = true;
+            this.btnAgregarTramo.Click += new System.EventHandler(this.btnAgregarTramo_Click);
+            // 
+            // btnCrearRecorrido
+            // 
+            this.btnCrearRecorrido.Location = new System.Drawing.Point(211, 358);
+            this.btnCrearRecorrido.Name = "btnCrearRecorrido";
+            this.btnCrearRecorrido.Size = new System.Drawing.Size(107, 23);
+            this.btnCrearRecorrido.TabIndex = 9;
+            this.btnCrearRecorrido.Text = "Crear recorrido";
+            this.btnCrearRecorrido.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(69, 358);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // dataGridViewTramos
+            // 
+            this.dataGridViewTramos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTramos.Location = new System.Drawing.Point(33, 226);
+            this.dataGridViewTramos.Name = "dataGridViewTramos";
+            this.dataGridViewTramos.Size = new System.Drawing.Size(313, 126);
+            this.dataGridViewTramos.TabIndex = 11;
             // 
             // CrearRecorrido
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 261);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(374, 393);
+            this.Controls.Add(this.dataGridViewTramos);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCrearRecorrido);
+            this.Controls.Add(this.btnAgregarTramo);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxDestino);
+            this.Controls.Add(this.comboBoxOrigen);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "CrearRecorrido";
             this.Text = "Crear Recorrido";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTramos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,7 +166,14 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxOrigen;
+        private System.Windows.Forms.ComboBox comboBoxDestino;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnAgregarTramo;
+        private System.Windows.Forms.Button btnCrearRecorrido;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dataGridViewTramos;
     }
 }
