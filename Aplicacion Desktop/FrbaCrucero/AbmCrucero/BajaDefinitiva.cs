efb2fb6f7b553a0fd3dac6e52aae81c29cd1489a
fedeDosSantos <fededs20@gmail.com>
2019-06-13 02:53:01 -0300

    baja de crucero

---
+++ Aplicacion Desktop/FrbaCrucero/AbmCrucero/BajaDefinitiva.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaCrucero.AbmCrucero
+{
+    public partial class BajaDefinitiva : Form
+    {
+        public DateTime fechaBajaDefinitiva;
+        public BajaDefinitiva()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBaja_Click(object sender, EventArgs e)
+        {
+            fechaBajaDefinitiva = Convert.ToDateTime(dateTimePickerBaja.Value.ToString());
+        }
+    }
+}
---
+++ Aplicacion Desktop/FrbaCrucero/AbmCrucero/Habilitacion.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace FrbaCrucero.AbmCrucero
+{
+    partial class Habilitacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewEstados = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnFueraDeServicio = new System.Windows.Forms.Button();
+            this.btnBajaDefinitiva = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEstados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEstado);
+            this.groupBox1.Controls.Add(this.txtModelo);
+            this.groupBox1.Controls.Add(this.txtMarca);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(565, 121);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Crucero";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(289, 90);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(125, 20);
+            this.txtEstado.TabIndex = 7;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(289, 64);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(125, 20);
+            this.txtModelo.TabIndex = 6;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(289, 38);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(125, 20);
+            this.txtMarca.TabIndex = 5;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(289, 13);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(125, 20);
+            this.txtId.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(154, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Estado actual";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Modelo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marca";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identificador";
+            // 
+            // dataGridViewEstados
+            // 
+            this.dataGridViewEstados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEstados.Location = new System.Drawing.Point(12, 215);
+            this.dataGridViewEstados.Name = "dataGridViewEstados";
+            this.dataGridViewEstados.ReadOnly = true;
+            this.dataGridViewEstados.Size = new System.Drawing.Size(565, 108);
+            this.dataGridViewEstados.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Estados del crucero";
+            // 
+            // btnFueraDeServicio
+            // 
+            this.btnFueraDeServicio.Location = new System.Drawing.Point(135, 163);
+            this.btnFueraDeServicio.Name = "btnFueraDeServicio";
+            this.btnFueraDeServicio.Size = new System.Drawing.Size(126, 23);
+            this.btnFueraDeServicio.TabIndex = 3;
+            this.btnFueraDeServicio.Text = "Poner fuera de servicio";
+            this.btnFueraDeServicio.UseVisualStyleBackColor = true;
+            this.btnFueraDeServicio.Click += new System.EventHandler(this.btnFueraDeServicio_Click);
+            // 
+            // btnBajaDefinitiva
+            // 
+            this.btnBajaDefinitiva.Location = new System.Drawing.Point(324, 163);
+            this.btnBajaDefinitiva.Name = "btnBajaDefinitiva";
+            this.btnBajaDefinitiva.Size = new System.Drawing.Size(126, 23);
+            this.btnBajaDefinitiva.TabIndex = 4;
+            this.btnBajaDefinitiva.Text = "Dar baja definitiva";
+            this.btnBajaDefinitiva.UseVisualStyleBackColor = true;
+            this.btnBajaDefinitiva.Click += new System.EventHandler(this.btnBajaDefinitiva_Click);
+            // 
+            // Habilitacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 336);
+            this.Controls.Add(this.btnBajaDefinitiva);
+            this.Controls.Add(this.btnFueraDeServicio);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridViewEstados);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Habilitacion";
+            this.Text = "Habilitacion";
+            this.Load += new System.EventHandler(this.Habilitacion_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEstados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewEstados;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnFueraDeServicio;
+        private System.Windows.Forms.Button btnBajaDefinitiva;
+    }
+}
