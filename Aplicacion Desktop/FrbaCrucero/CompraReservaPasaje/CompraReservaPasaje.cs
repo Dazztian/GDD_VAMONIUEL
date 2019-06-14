2084095852b27732f05d939b74bda96319af1a71
Federico Hazama <fehc97@hotmail.com>
2019-06-14 06:45:53 -0300

    cargo dgv correctamente filtrada

--- Aplicacion Desktop/FrbaCrucero/CompraReservaPasaje/CompraReservaPasaje.cs
+++ Aplicacion Desktop/FrbaCrucero/CompraReservaPasaje/CompraReservaPasaje.cs
@@ -37,7 +37,18 @@ namespace FrbaCrucero.CompraPasaje
 
         private void btnBuscarviajes_Click(object sender, EventArgs e)
         {
+            Dictionary<string, string> filtros = this.ArmaFiltro(cmbOrigen.Text, cmbDestino.Text, dtpFechaviaje.Value.ToString("yyyy/MM/dd"));
+            Conexion.getInstance().LlenarDataGridView(Conexion.Tabla.viaje_oyd, ref dgv, filtros);
+        }
 
+        private Dictionary<string, string> ArmaFiltro(string origen, string destino, string fecha)
+        {
+            Dictionary<string, string> filtros = new Dictionary<string, string>();
+            filtros.Add("Origen", Conexion.Filtro.Exacto(origen));
+            filtros.Add("Destino", Conexion.Filtro.Exacto(destino));
+            filtros.Add("FechaInicio", Conexion.Filtro.Exacto(fecha));
+        
+            return filtros;
         }
     }
 }
