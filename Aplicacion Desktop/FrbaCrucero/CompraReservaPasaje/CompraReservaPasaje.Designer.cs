b69162a28c17d8f274644592afb1024b4c9659aa
Federico Hazama <fehc97@hotmail.com>
2019-06-14 05:06:38 -0300

    genero compra reserva pasaje
    creo formulario y precargocombos

---
+++ Aplicacion Desktop/FrbaCrucero/CompraReservaPasaje/CompraReservaPasaje.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace FrbaCrucero.CompraPasaje
+{
+    partial class CompraReservaPasaje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpFechaviaje = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbOrigen = new System.Windows.Forms.ComboBox();
+            this.cmbDestino = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmbViaje = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnElegirviaje = new System.Windows.Forms.Button();
+            this.btnBuscarviajes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha de viaje";
+            // 
+            // dtpFechaviaje
+            // 
+            this.dtpFechaviaje.Location = new System.Drawing.Point(116, 24);
+            this.dtpFechaviaje.Name = "dtpFechaviaje";
+            this.dtpFechaviaje.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaviaje.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puerto de origen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Puerto destino";
+            // 
+            // cmbOrigen
+            // 
+            this.cmbOrigen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOrigen.FormattingEnabled = true;
+            this.cmbOrigen.Location = new System.Drawing.Point(124, 60);
+            this.cmbOrigen.Name = "cmbOrigen";
+            this.cmbOrigen.Size = new System.Drawing.Size(192, 21);
+            this.cmbOrigen.TabIndex = 4;
+            // 
+            // cmbDestino
+            // 
+            this.cmbDestino.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDestino.FormattingEnabled = true;
+            this.cmbDestino.Location = new System.Drawing.Point(124, 95);
+            this.cmbDestino.Name = "cmbDestino";
+            this.cmbDestino.Size = new System.Drawing.Size(192, 21);
+            this.cmbDestino.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(602, 378);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // cmbViaje
+            // 
+            this.cmbViaje.FormattingEnabled = true;
+            this.cmbViaje.Location = new System.Drawing.Point(116, 541);
+            this.cmbViaje.Name = "cmbViaje";
+            this.cmbViaje.Size = new System.Drawing.Size(121, 21);
+            this.cmbViaje.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 548);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ID de viaje";
+            // 
+            // btnElegirviaje
+            // 
+            this.btnElegirviaje.Location = new System.Drawing.Point(484, 539);
+            this.btnElegirviaje.Name = "btnElegirviaje";
+            this.btnElegirviaje.Size = new System.Drawing.Size(75, 23);
+            this.btnElegirviaje.TabIndex = 9;
+            this.btnElegirviaje.Text = "Elegir viaje";
+            this.btnElegirviaje.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarviajes
+            // 
+            this.btnBuscarviajes.Location = new System.Drawing.Point(381, 95);
+            this.btnBuscarviajes.Name = "btnBuscarviajes";
+            this.btnBuscarviajes.Size = new System.Drawing.Size(104, 23);
+            this.btnBuscarviajes.TabIndex = 10;
+            this.btnBuscarviajes.Text = "Buscar viajes";
+            this.btnBuscarviajes.UseVisualStyleBackColor = true;
+            this.btnBuscarviajes.Click += new System.EventHandler(this.btnBuscarviajes_Click);
+            // 
+            // CompraReservaPasaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 644);
+            this.Controls.Add(this.btnBuscarviajes);
+            this.Controls.Add(this.btnElegirviaje);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbViaje);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbDestino);
+            this.Controls.Add(this.cmbOrigen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpFechaviaje);
+            this.Controls.Add(this.label1);
+            this.Name = "CompraReservaPasaje";
+            this.Text = "CompraReservaPasaje";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpFechaviaje;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbOrigen;
+        private System.Windows.Forms.ComboBox cmbDestino;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbViaje;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnElegirviaje;
+        private System.Windows.Forms.Button btnBuscarviajes;
+    }
+}
---
+++ Aplicacion Desktop/FrbaCrucero/CompraReservaPasaje/CompraReservaPasaje.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaCrucero.CompraPasaje
+{
+    public partial class CompraReservaPasaje : Form
+    {
+        public CompraReservaPasaje()
+        {
+            InitializeComponent();
+            dtpFechaviaje.MinDate = ConfigurationHelper.fechaActual;
+            List<string> columnas = new List<string>();
+            columnas.Add("Nombre");
+            List<object> resultadoConsulta = ((Conexion.getInstance().ConsultaPlana(Conexion.Tabla.Puerto, columnas, null))["Nombre"]);
+            resultadoConsulta.Sort();
+
+            for (int i=0; i<resultadoConsulta.Count() ; i++)
+            {
+                cmbOrigen.Items.Add(resultadoConsulta[i].ToString());
+            }
+            cmbOrigen.Text = resultadoConsulta[0].ToString();
+ 
+            for (int i=0; i<resultadoConsulta.Count() ; i++)
+            {
+                cmbDestino.Items.Add(resultadoConsulta[i].ToString());
+            }
+            cmbDestino.Text = resultadoConsulta[0].ToString();
+
+        }
+
+        private void btnBuscarviajes_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
