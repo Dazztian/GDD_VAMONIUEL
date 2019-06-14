950253250d13d3f9087a6de5faf66a2a2bb1066c
Dazztian <diegodazpeitia@gmail.com>
2019-06-14 04:22:19 -0300

    arreglado coso del formtable

--- Aplicacion Desktop/FrbaCrucero/CompraReservaPasaje/Form1.cs
+++ Aplicacion Desktop/FrbaCrucero/CompraReservaPasaje/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace FrbaCrucero.CompraPasaje
 {
-    public partial class Form1 : FormTemplate
+    public partial class Form1 : Form//Template
     {
         public Form1()
         {
