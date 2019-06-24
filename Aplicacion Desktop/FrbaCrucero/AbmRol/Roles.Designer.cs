ad2369c56ae1c8d307bd0c5420f60099df25c729
fedeDosSantos <fededs20@gmail.com>
2019-06-24 18:20:11 -0300

    AbmRol: actualiza tabla y busqueda

--- Aplicacion Desktop/FrbaCrucero/AbmRol/Roles.Designer.cs
+++ Aplicacion Desktop/FrbaCrucero/AbmRol/Roles.Designer.cs
@@ -33,20 +33,23 @@
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnHabilitar = new System.Windows.Forms.Button();
             this.btnDeshabilitar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRoles)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewRoles
             // 
             this.dataGridViewRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewRoles.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewRoles.Location = new System.Drawing.Point(12, 121);
             this.dataGridViewRoles.Name = "dataGridViewRoles";
             this.dataGridViewRoles.Size = new System.Drawing.Size(404, 150);
             this.dataGridViewRoles.TabIndex = 0;
             // 
             // btnAgregarRol
             // 
-            this.btnAgregarRol.Location = new System.Drawing.Point(12, 211);
+            this.btnAgregarRol.Location = new System.Drawing.Point(12, 296);
             this.btnAgregarRol.Name = "btnAgregarRol";
             this.btnAgregarRol.Size = new System.Drawing.Size(75, 23);
             this.btnAgregarRol.TabIndex = 1;
@@ -56,7 +59,7 @@
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(119, 211);
+            this.btnModificar.Location = new System.Drawing.Point(119, 296);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(75, 23);
             this.btnModificar.TabIndex = 2;
@@ -66,7 +69,7 @@
             // 
             // btnHabilitar
             // 
-            this.btnHabilitar.Location = new System.Drawing.Point(237, 210);
+            this.btnHabilitar.Location = new System.Drawing.Point(237, 295);
             this.btnHabilitar.Name = "btnHabilitar";
             this.btnHabilitar.Size = new System.Drawing.Size(75, 24);
             this.btnHabilitar.TabIndex = 3;
@@ -76,7 +79,7 @@
             // 
             // btnDeshabilitar
             // 
-            this.btnDeshabilitar.Location = new System.Drawing.Point(341, 210);
+            this.btnDeshabilitar.Location = new System.Drawing.Point(341, 295);
             this.btnDeshabilitar.Name = "btnDeshabilitar";
             this.btnDeshabilitar.Size = new System.Drawing.Size(75, 23);
             this.btnDeshabilitar.TabIndex = 4;
@@ -84,11 +87,40 @@
             this.btnDeshabilitar.UseVisualStyleBackColor = true;
             this.btnDeshabilitar.Click += new System.EventHandler(this.btnDeshabilitar_Click);
             // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(194, 82);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(169, 43);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 7;
+            // 
             // Roles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 249);
+            this.ClientSize = new System.Drawing.Size(428, 333);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.btnDeshabilitar);
             this.Controls.Add(this.btnHabilitar);
             this.Controls.Add(this.btnModificar);
@@ -99,6 +131,7 @@
             this.Load += new System.EventHandler(this.Roles_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRoles)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,5 +142,8 @@
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnHabilitar;
         private System.Windows.Forms.Button btnDeshabilitar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
     }
 }
