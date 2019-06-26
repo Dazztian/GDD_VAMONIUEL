966cb6f258af2ba0c491141abd940b6d0ffeb160
Dazztian <diegodazpeitia@gmail.com>
2019-06-26 16:44:46 -0300

    cambie cerrar sesion x ir a menu ppal

--- Aplicacion Desktop/FrbaCrucero/FormTemplate.Designer.cs
+++ Aplicacion Desktop/FrbaCrucero/FormTemplate.Designer.cs
@@ -56,7 +56,7 @@
             this.cerrarSesionToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.cerrarSesionToolStripMenuItem.Name = "cerrarSesionToolStripMenuItem";
             this.cerrarSesionToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
-            this.cerrarSesionToolStripMenuItem.Text = "Cerrar Sesion";
+            this.cerrarSesionToolStripMenuItem.Text = "Volver a pantalla inicial";
             this.cerrarSesionToolStripMenuItem.Click += new System.EventHandler(this.cerrarSesionToolStripMenuItem_Click);
             // 
             // FormTemplate
